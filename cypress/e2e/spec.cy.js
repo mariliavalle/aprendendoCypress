f6d51c274c9bd6e6aa3785d68c9b0f6597f49e10
Marilia do Valle <mdovallerocha@gmail.com>
2025-03-24 09:01:38 -0300

    cadastrar segundo usuario

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,8 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
     cy.contains('a', 'Cadastrar').click();
-    cy.get('input[name="nome"]').type('Carlos');
-    cy.get('input[name="email"]').type('carlos@email.com');
+    cy.get('input[name="nome"]').type('Lucas');
+    cy.get('input[name="email"]').type('lucas@email.com');
     cy.get('input[name="password"]').type('Teste123');
     cy.get('input[name="confirm_password"]').type('Teste123');
     cy.contains('button', 'Cadastrar').click();
