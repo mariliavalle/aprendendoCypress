5f689f7032d5e46f72084fea8aa33702fc0eae9f
Marilia do Valle <mdovallerocha@gmail.com>
2025-03-31 12:15:28 -0300

    Protegendo dados sensiveis nos testes

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,12 +1,9 @@
 describe('API Adopet', () => {
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTc0MzAxNzM3MiwiZXhwIjoxNzQzMjc2NTcyfQ.BKzk2kocxKlrqH5EAI1KuTNlhErCCAeJ9F5j6zieMOY`
-
-
-    it('Mensagem da API', () => {
+        it('Mensagem da API', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c' ,
-            headers: { authorization }
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
